6bc57a841fb97c3293ec45ec3f960b69b7fd492b
therealkh <uktam4ik.khamidov@gmail.com>
2020-08-28 14:28:36 +0500

    табы виджета новостей готовы

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,14 +5,17 @@ $(document).ready(function () {
     autoplaySpeed: 6500,
   });
 });
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
+
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu-block');
+
+
+  // **********[ANY CLICK ON PAGE]*************
   document.addEventListener('click', (event) => {
+
+
+    //! ***********[BURGER]*********** 
     if (event.target.closest('.burger')) {
       burger.classList.toggle('active');
       if (burger.classList.contains('active')) {
@@ -36,8 +39,32 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.style.display = 'none';
       }, 300); //! таймаут должен совпадать с transition
     }
-  });
+    //! ***********[BURGER END]***********
 
+    //! **************[TABS]**************
+    if (event.target.classList.contains('tab-nav')) {
+      const currentTabNav = event.target;
+      const parent = event.target.parentElement;
+      const tabNav = parent.querySelectorAll('.tab-nav');
+      const tabNavID = currentTabNav.getAttribute('data-tab-nav-id');
+      const tabContent = parent.parentElement.parentElement.querySelector('.tab-content-wrapper').querySelectorAll('.tab-content');
+      //console.log(tabContent);
+      tabNav.forEach((item) => {
+        item.classList.remove('tab-nav-active');
+      });
+      currentTabNav.classList.add('tab-nav-active');
+      tabContent.forEach((item) => {
+        item.classList.remove('tab-content-active');
+        if (item.getAttribute('data-tab-content-id') === tabNavID) {
+          item.classList.add('tab-content-active');
+        }
+      });
+    }
+    //! ************[TABS END]************
+
+
+  });
+  /* ***************[возврат меню в хедер при изминении ширины окна]************ */
   window.addEventListener('resize', (event) => {
     width = event.target.innerWidth;
     if (width > 1080) {
@@ -46,4 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
       menu.classList.remove('active');
     }
   });
+
+
+
 })
